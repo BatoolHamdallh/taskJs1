ea881895a0bcfb9341256e060fd9b6773648ec8a
Batool Hamdallh <batoul.hamdallh@gmail.com>
2024-02-04 17:57:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//add class demo to div in html
+var element = document.querySelector('div');
+console .log (element);
+element.classList.add('demo');
+// add class vip to h1 in html then remove it 
+ var h1 = document.querySelector('h1');
+ console .log (h1);
+h1.classList.add('vip');
+h1.classList.remove('vip');
